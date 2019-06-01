5c1c8eb2a0ccf04359db0ddf9a3eeac5d0c28ef3
Roland_McDoland <miki.franek@gmail.com>
2019-06-01 21:42:55 +0200

    Lab6 update

--- Lab6/app/src/main/java/com/example/smartphoneinfo/MainActivity.kt
+++ Lab6/app/src/main/java/com/example/smartphoneinfo/MainActivity.kt
@@ -1,18 +1,38 @@
 package com.example.smartphoneinfo
 
+import android.content.Context
 import android.content.pm.PackageManager
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.HardwarePropertiesManager
+import android.os.HardwarePropertiesManager.DEVICE_TEMPERATURE_CPU
+import android.os.HardwarePropertiesManager.TEMPERATURE_CURRENT
 import android.support.v4.app.ActivityCompat
+import android.support.v4.app.NotificationCompat
 import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : AppCompatActivity(), SensorEventListener {
     private var locationManager : LocationManager? = null
+
+    private var lightSensor : Sensor? = null
+    private var sensorManager : SensorManager? = null
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        lightingTextView.text = "Lighting sensor: " + event!!.values[0]
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +53,8 @@ class MainActivity : AppCompatActivity() {
             Log.d("myTag", "Security Exception, no location available")
         }
 
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager?
+        lightSensor = sensorManager!!.getDefaultSensor(Sensor.TYPE_LIGHT)
     }
 
     //define the listener
@@ -43,5 +65,22 @@ class MainActivity : AppCompatActivity() {
         override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
         override fun onProviderEnabled(provider: String) {}
         override fun onProviderDisabled(provider: String) {}
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        sensorManager!!.registerListener(this, lightSensor, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        sensorManager!!.unregisterListener(this)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        //TODO show notification
     }
 }
