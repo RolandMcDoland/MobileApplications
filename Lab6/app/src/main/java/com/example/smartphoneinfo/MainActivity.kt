a51d269293f9ad580bf804da2035dc402f5dfe22
Roland_McDoland <miki.franek@gmail.com>
2019-06-01 22:02:01 +0200

    Lab6 update

--- Lab6/app/src/main/java/com/example/smartphoneinfo/MainActivity.kt
+++ Lab6/app/src/main/java/com/example/smartphoneinfo/MainActivity.kt
@@ -1,7 +1,14 @@
 package com.example.smartphoneinfo
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.graphics.Color
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
@@ -9,15 +16,12 @@ import android.hardware.SensorManager
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.os.HardwarePropertiesManager
-import android.os.HardwarePropertiesManager.DEVICE_TEMPERATURE_CPU
-import android.os.HardwarePropertiesManager.TEMPERATURE_CURRENT
 import android.support.v4.app.ActivityCompat
-import android.support.v4.app.NotificationCompat
 import android.util.Log
-import android.widget.Toast
+import android.widget.RemoteViews
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
@@ -25,6 +29,12 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 
     private var lightSensor : Sensor? = null
     private var sensorManager : SensorManager? = null
+
+    lateinit var notificationManager : NotificationManager
+    lateinit var notificationChannel : NotificationChannel
+    lateinit var builder : Notification.Builder
+    private val channelId = "com.example.smartphoneinfo"
+    private val description = "Still running in the background"
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
 
@@ -55,6 +65,8 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager?
         lightSensor = sensorManager!!.getDefaultSensor(Sensor.TYPE_LIGHT)
+
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
     }
 
     //define the listener
@@ -81,6 +93,37 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 
     override fun onStop() {
         super.onStop()
-        //TODO show notification
+
+        notifyAboutBackgroundRun()
     }
+
+    fun notifyAboutBackgroundRun() {
+
+        val intent = Intent(this,MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_UPDATE_CURRENT)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notificationChannel = NotificationChannel(channelId,description,NotificationManager.IMPORTANCE_HIGH)
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor = Color.BLUE
+            notificationChannel.enableVibration(false)
+            notificationManager.createNotificationChannel(notificationChannel)
+
+            builder = Notification.Builder(this,channelId)
+                .setContentTitle("Caution")
+                .setContentText("SmartphoneInfo still running in the background!")
+                .setSmallIcon(R.drawable.sw_icon)
+                .setContentIntent(pendingIntent)
+        }else{
+
+            builder = Notification.Builder(this)
+                .setContentTitle("Caution")
+                .setContentText("SmartphoneInfo still running in the background!")
+                .setSmallIcon(R.drawable.sw_icon)
+                .setContentIntent(pendingIntent)
+        }
+
+        notificationManager.notify(1234,builder.build())
+    }
+
 }
